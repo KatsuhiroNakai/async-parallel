be89fc7c186b1535de42c783cf40ee26b6ef1e8b
Katsuhiro Nakai <katsuhiro.nakai@kansei-ai.com>
2024-12-11 21:50:50 +0900

    表示変更

--- src/async_sample.py
+++ src/async_sample.py
@@ -16,22 +16,30 @@ async def wait_one_second_async():
     await asyncio.sleep(1)
 
 
-def sync_main():
-    """同期処理で1秒待つタスクを10回する"""
+def sync_main() -> float:
+    """
+    同期処理で1秒待つタスクを10回する
+    """
     start_time = time.perf_counter()
     for _ in tqdm(range(10), desc="Sync Main"):
         wait_one_second()
-    logger.info(f"Sync Main End: {time.perf_counter() - start_time} [s]")
+
+    process_time = time.perf_counter() - start_time
+    logger.info(f"Sync Main End: {process_time} [s]")
+    return process_time
 
 
-async def async_main():
+async def async_main() -> float:
     """非同期処理で1秒待つタスクを10回する"""
     start_time = time.perf_counter()
     await asyncio.gather(*(wait_one_second_async() for _ in range(10)))
-    logger.info(f"Async Main End: {time.perf_counter() - start_time} [s]")
+
+    process_time = time.perf_counter() - start_time
+    logger.info(f"Async Main End: {process_time} [s]")
+    return process_time
 
 
-async def async_main_with_progress():
+async def async_main_with_progress() -> float:
     """
     非同期処理で1秒待つタスクを10回する
     進捗表示あり
@@ -40,10 +48,13 @@ async def async_main_with_progress():
     tasks = [wait_one_second_async() for _ in range(10)]
     for f in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc="Async Progress"):
         await f
-    logger.info(f"Async Main with Progress End: {time.perf_counter() - start_time} [s]")
+
+    process_time = time.perf_counter() - start_time
+    logger.info(f"Async Main with Progress End: {process_time} [s]")
+    return process_time
 
 
-async def async_main_use_tasks_list():
+async def async_main_use_tasks_list() -> float:
     """非同期処理で1秒末タスクを10回実行する
     tasksリストを使ってタスクリストを作成し、非同期実行する
     """
@@ -51,28 +62,42 @@ async def async_main_use_tasks_list():
     tasks = []
     for _ in range(10):
         tasks.append(wait_one_second_async())
-
     await asyncio.gather(*tasks)
-    logger.info(f"Async Main use Tasks List End: {time.perf_counter() - start_time} [s]")
+
+    process_time = time.perf_counter() - start_time
+    logger.info(f"Async Main use Tasks List End: {process_time} [s]")
+    return process_time
 
 
-def async_to_sync():
+def async_to_sync() -> float:
     """
     非同期関数を同期関数に変換する
     shapなど同期関数しか使えないライブラリを使う場合に使用する
     """
     start_time = time.perf_counter()
     asyncio.run(async_main())
-    logger.info(f"Async to Sync End: {time.perf_counter() - start_time} [s]")
+
+    process_time = time.perf_counter() - start_time
+    logger.info(f"Async to Sync End: {process_time} [s]")
+    return process_time
 
 
 if __name__ == "__main__":
-    sync_main()
+    sync_time = sync_main()
     print("----")
-    asyncio.run(async_main())
+    async_time1 = asyncio.run(async_main())
     print("----")
-    asyncio.run(async_main_with_progress())
+    async_time2 = asyncio.run(async_main_with_progress())
     print("----")
-    asyncio.run(async_main_use_tasks_list())
+    async_time3 = asyncio.run(async_main_use_tasks_list())
     print("----")
-    async_to_sync()
+    async_time4 = async_to_sync()
+
+    print("Sync ---")
+    print(f"{sync_time} [s]")
+
+    print("Async ---")
+    print(f"{async_time1} [s]")
+    print(f"{async_time2} [s]")
+    print(f"{async_time3} [s]")
+    print(f"{async_time4} [s]")
