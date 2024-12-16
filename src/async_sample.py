36cad011309c15fe897078e5cdf9036d517da368
Katsuhiro Nakai <katsuhiro.nakai@kansei-ai.com>
2024-12-16 21:09:56 +0900

    asyncでwait_time秒待つように変更

--- src/async_sample.py
+++ src/async_sample.py
@@ -13,16 +13,16 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def wait_one_second():
-    """1秒待つ"""
-    time.sleep(1)
+def wait_one_second(wait_time: int):
+    """wait_time秒待つ"""
+    time.sleep(wait_time)
 
 
-async def wait_one_second_async():
-    """1秒待つ
+async def wait_one_second_async(wait_time: int):
+    """wait_time秒待つ
     非同期処理用
     """
-    await asyncio.sleep(1)
+    await asyncio.sleep(wait_time)
 
 
 # 同期処理
@@ -32,7 +32,7 @@ def sync_main() -> float:
     """
     start_time = time.perf_counter()
     for _ in tqdm(range(10), desc="Sync Main"):
-        wait_one_second()
+        wait_one_second(1)
 
     process_time = time.perf_counter() - start_time
     logger.info(f"Sync Main End: {process_time} [s]")
@@ -47,7 +47,7 @@ async def async_main() -> float:
     """
     start_time = time.perf_counter()
     # wait_one_second_async()を10回分用意して、*で展開して非同期処理を実行
-    await asyncio.gather(*(wait_one_second_async() for _ in range(10)))
+    await asyncio.gather(*(wait_one_second_async(1) for _ in range(10)))
 
     process_time = time.perf_counter() - start_time
     logger.info(f"Async Main End: {process_time} [s]")
@@ -60,7 +60,7 @@ async def async_main_with_progress() -> float:
     進捗表示あり
     """
     start_time = time.perf_counter()
-    tasks = [wait_one_second_async() for _ in range(10)]
+    tasks = [wait_one_second_async(1) for _ in range(10)]
     # tqdmで進捗表示
     for f in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc="Async Progress"):
         await f
@@ -79,7 +79,7 @@ async def async_main_use_tasks_list() -> float:
     # タスクリストを作成
     tasks = []
     for _ in range(10):
-        tasks.append(wait_one_second_async())
+        tasks.append(wait_one_second_async(1))
     # *で展開して非同期処理を実行
     await asyncio.gather(*tasks)
 
